86c6c6e6c86176aa6cb8c66a4e23bb9ec455dd37
YevhenPetrovskyi <71007428+YevhenPetrovskyi@users.noreply.github.com>
2024-02-01 13:47:38 +0200

    add TodoActions component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import TodoForm from './components/Todos/TodoForm';
+import TodoActions from './components/Todos/TodoActions';
 
 import './App.css';
 
@@ -17,6 +18,32 @@ function App() {
 
     setTodos([...todos, newTodo]);
   };
+
+  const deleteTodoHandler = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const toggleTodoHandler = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id
+          ? { ...todo, isCompleted: !todo.isCompleted }
+          : { ...todo }
+      )
+    );
+  };
+
+  const resetTodoHandler = () => {
+    setTodos([]);
+  };
+
+  const deleteCompletedTodosHandler = () => {
+    setTodos(todos.filter(({ isCompleted }) => !isCompleted));
+  };
+
+  const completedTodosCount = todos.filter(
+    ({ isCompleted }) => isCompleted
+  ).length;
 
   return (
     <div className="App">
