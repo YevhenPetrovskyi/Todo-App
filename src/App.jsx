b6dcd0ae2ea3aaa5892a5861bbfa9d0a762549cf
YevhenPetrovskyi <71007428+YevhenPetrovskyi@users.noreply.github.com>
2024-02-01 13:00:28 +0200

    add Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodoHandler = (text) => {
+    const newTodo = {
+      text,
+      isCompleted: false,
+      id: uuidv4(),
+    };
+
+    setTodos([...todos, newTodo]);
+  };
+
   return (
     <div className="App">
       <h1>Todo App</h1>
